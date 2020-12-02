d99cd38e124cdddf15703e41e6a6963e3f503d5b
Ryan  Yim <ryanchichun.yim23@ycdsbk12.ca>
2020-12-02 20:18:03 +0000

    Completed 2

---
+++ oz_to_ml.py
@@ -0,0 +1,4 @@
+oz= float(input("Enter the amount of ounces:"))
+People = float(input("Enter the people to serve:"))
+oz_to_ml= ((oz*29.57)* People)
+print("This is how much ml you will need:" + str(oz_to_ml))
